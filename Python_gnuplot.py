021dc29c1396f6f8efed1fbc123d7d7ccdb4b07c
czjiangjun <czjiangjun@gmail.com>
2020-10-16 17:41:20 +0800

    coreden_wavef

--- Python_gnuplot.py
+++ Python_gnuplot.py
@@ -39,13 +39,12 @@ v
 c
 v
 1
-1.7   1.5   1.7    1.0
+1.85  1.3   1.5    1.5
 y
--9.61
+-0.50
 n
 y
-0.57
-n
+0.22
 n
 vasprrkj  gramschmidtortho  Besselshape
 1 2   MTROULLIER
@@ -66,23 +65,48 @@ END\n  '''
       file_handle.flush()
       file_handle.close()
 
-def Data_AE_Fig( data, type, Properties):
-    TYPE1 = "VASP-"+type
-    TYPE2 = "ATOM-"+type
+def Data_Fig(Properties, type, data, name):
+    TYPE = type+"-"+data
+#    TYPE2 = "ATOM-"+type
 
-    plot_command_VASP = "plot "+'"VASP_'+data +'" '+'u 1:2 w p pt 3 ps 1.2 title "'+TYPE1 +'"'
+    plot_command = "plot "+'"'+type+'_'+data +'" '+'u 1:2 w p pt 3 ps 1.2 title "'+TYPE +'"'
+#    plot_command_ATOM = "replot "+'"ATOM_'+data +'" '+'u 1:2 w l lt 5 lw 1.8 title "'+TYPE2+'"'
+#    print(plot_command_VASP)
+#    exit ()
+    Fig_title = "set title "+ '"' + data +'"'
+    Fig_name = type+"_"+name+".eps"
+
+    gp.c(Fig_title)
+    if (Properties == "wavefunction"):
+       gp.c('set xrange[0:1.5]')
+       gp.c('set yrange[-1.5:1.5]')
+
+    gp.c(plot_command)
+    # gp.c('plot "VASP_WAE" u 1:2 w p pt 3 ps 2 title "VASP-AE"')
+    # gp.c(plot_command_ATOM)
+    # gp.p('myfigure.ps')
+    gp.p(Fig_name)
+
+def Data_compare_Fig(Properties, data, name):
+    TYPE1 = "VASP-"+data
+    TYPE2 = "ATOM-"+data
+
+    plot_command_VASP = "plot "+'"VASP_'+data +'" '+'u 1:2 w p pt 3 lw 1.2 title "'+TYPE1+'"'
     plot_command_ATOM = "replot "+'"ATOM_'+data +'" '+'u 1:2 w l lt 5 lw 1.8 title "'+TYPE2+'"'
 #    print(plot_command_VASP)
 #    exit ()
     Fig_title = "set title "+ '"' + data +'"'
-    Fig_name = Properties+".eps"
+    Fig_name = name+".eps"
 
     gp.c(Fig_title)
-    gp.c('set xrange[0:1.5]')
-    gp.c('set yrange[-1.5:1.5]')
+    if (Properties == "wavefunction"):
+       gp.c('set xrange[0:1.5]')
+       gp.c('set yrange[-1.5:1.5]')
+    gp.c('set yrange[-50:100]')
     gp.c(plot_command_VASP)
-    # gp.c('plot "VASP_WAE" u 1:2 w p pt 3 ps 2 title "VASP-AE"')
     gp.c(plot_command_ATOM)
+    # gp.c('plot "VASP_WAE" u 1:2 w p pt 3 ps 2 title "VASP-AE"')
+    # gp.c(plot_command_ATOM)
     # gp.p('myfigure.ps')
     gp.p(Fig_name)
 
@@ -91,10 +115,14 @@ def main():
 
     os.system("~/Softs/atompaw_test/src/atompaw < in > out")
     #exit ()
-    time.sleep(5)
+#    time.sleep(5)
 
-    Data_AE_Fig("WAE", "AE", "WAE_data")
-    # Data_AE_Fig("CORE", "AE", "CORE_data")
+#    Data_Fig("potential", "VASP", "POTAE", "POTAE_data")
+#    Data_compare_Fig("density", "CORE", "CORE_data")
+#    Data_compare_Fig("density", "PCORE", "PCORE_data")
+#    Data_compare_Fig("wavefunction", "WAE", "WAE_data")
+#    Data_compare_Fig("wavefunction", "WPS", "WPS_data")
+    Data_compare_Fig("potential", "POTAE", "POTAE_data")
 
 if __name__ == '__main__':
     main()
